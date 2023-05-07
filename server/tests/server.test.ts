003a5db96dc37e8acee0e5d6a5dab04da3557029
Kasulejoseph <kasule06joseph@gmail.com>
2023-05-07 17:26:30 +0300

    Add unit tests

---
+++ server/tests/server.test.ts
@@ -0,0 +1,45 @@
+import api from "../api";
+import { beforeAll, afterAll, describe, expect, it } from "@jest/globals";
+
+beforeAll(() => api.listen({ port: 4000 }));
+afterAll(() => api.stop());
+
+describe("GraphQL API", () => {
+  it("should return a list of people", async () => {
+    const response = await api.executeOperation({
+      query: `
+      query {
+        people {
+          name
+          height
+          mass
+          homeworld
+          gender
+        }
+      }
+    `,
+    });
+    expect(response.errors).toBeUndefined();
+    expect(response.data).toHaveProperty("people");
+    expect(response.data?.people).toBeInstanceOf(Array);
+  });
+
+  it("should return a person by name", async () => {
+    const response = await api.executeOperation({
+      query: `
+    query {
+      person(name: "R5-D4") {
+        name,
+        height,
+        mass,
+        homeworld,
+        gender
+      }
+    }
+  `,
+    });
+    expect(response.data).toHaveProperty("person");
+    expect(response.data?.person).toBeInstanceOf(Array);
+    expect(response.data?.person[0].name).toEqual("R5-D4");
+  });
+});
